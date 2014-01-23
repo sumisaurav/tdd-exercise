a65b2f828b8bd34fe11a7b54b28dee59c5d67249
Sumit Saurav <sumit24.s@tcs.com>
2014-01-23 03:42:33 +0200

    Check if there is a pair in a hand of cards

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
 var should = require("should");
 
-describe('hand', function(){
-	describe('with a pair', function() {
-		it('should return true',function(){
-			isPair([1,2,3,5,5]).should.equal(true);
-		});
-	});                       
+describe('pair', function(){
+    it('should contain two cards of the same rank',function(){
+		isPair([1,2,3,5,5]).should.equal(true);
+		isPair([1,2,3,4,5]).should.equal(false);
+		isPair([1,2,2,2,3]).should.equal(false);
+	});
 });
 
 var isPair = function(handOfCards){
-    return true;
+    var countArr = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+    for (var index = 0; index < handOfCards.length; index++) {
+        countArr[handOfCards[index] - 1]++;
+    }
+
+    for (var index = 0; index < countArr.length; index++){
+        if( countArr[index]==2)
+            return true;
+    }
+
+    return false;
 }
+
